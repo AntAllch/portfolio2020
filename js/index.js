841e8b4594e406fde33c29ea315da33b6639b087
AntAllch <anthonyallchorne@gmail.com>
2020-02-07 17:28:11 +0000

    Finished

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-link");
 
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
+});
+
+navLinks.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
 });
