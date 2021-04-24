0e8be0d77bf02e38f50a9cebdf2dc73f0c3f6906
AntAllch <anthonyallchorne@gmail.com>
2021-04-24 20:07:46 +0100

    added padding around anchors

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
+//Nav appear on scroll up
 var lastScrollTop = 0;
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollTop = window.pageYOffset;
 
     if(scrollTop > lastScrollTop){
         header.style.top = "-20%";
@@ -12,3 +13,17 @@ window.addEventListener("scroll", () => {
 
     lastScrollTop = scrollTop;
 })
+
+
+//Parallax BG
+$(window).scroll(function() {
+    parallax();
+})
+
+function parallax(){
+
+    var wScroll = $(window).scrollTop();
+
+    $(".parallax-bg").css("background-position", "center "+(wScroll*0.5)+"px")
+
+}
